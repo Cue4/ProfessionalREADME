8a9e45dc845e8d58d73df21dd0ab24379d9a1ab2
Caliph Ferguson <Quasar@Caliphs-Mini.lan>
2024-07-07 14:42:40 -0400

    working on generate readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license)
+  if(license !== "NONE"){
+    return `![github license](https://img.shields.io/badge/license-${license.replace(' ','_')}-red.svg)`
+  }else {
+    return ""
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "NONE"){
+    return `* [license](#license)`
+  }else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== "NONE"){
+    return `
+     
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+    `
+  }else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
- 
+ ${renderLicenseBadge(data.license)}
+
+### Table of Contents
+
+${renderLicenseLink(data.license)}
+
   ## Description
   
   ${data.briefDescription}
@@ -37,24 +76,11 @@ function generateMarkdown(data) {
   ${data.license}
   
   ---
+ 
+  ## License
   
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
-  ## Features
-  
-  If your project has a lot of features, list them here.
-  
-  ## How to Contribute
-  
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-  
-  ## Tests`
+  ${renderLicenseSection(data.license)}
+  `
 
 }
 
